57a89e8cdf3422670c60290892d3f48617695978
bonnysid <bonnysidworker@gmail.com>
2021-02-08 21:20:49 +0400

    add cheer

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/components/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/js'
+    },
+    watch: true,
+  
+    devtool: "source-map",
+  
+    module: {}
+};
